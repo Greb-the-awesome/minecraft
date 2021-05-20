622cc649b6135db7fb4c90f4ac10e5b2bbe74ea6
Greb-the-awesome <gyourselfolden@gmail.com>
2021-05-20 16:17:59 -0400

    initial commit

---
+++ blockInfo.py
@@ -0,0 +1,32 @@
+BLOCKS={
+    "GRASS" : {
+        "special" : True,
+        "texture-coords-top" : (
+            (0, 0.9),
+            (0.1, 0.9),
+            (0.1, 1),
+            (0,1)
+        ),
+        "texture-coords-sides" : (
+            (0.1, 0.9),
+            (0.2, 0.9),
+            (0.2, 1),
+            (0.1, 1)
+        ),
+        "texture-coords-bottom" : (
+            (0.2, 0.9),
+            (0.3, 0.9),
+            (0.3, 1),
+            (0.2, 1)
+        ),
+    },
+    "DIRT" : {
+        "special" : False,
+        "texture-coords" : (
+            (0.2, 0.9),
+            (0.3, 0.9),
+            (0.3, 1),
+            (0.2, 1)
+        )
+    }
+}
